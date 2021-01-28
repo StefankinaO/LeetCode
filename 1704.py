243280e573dbb30cea4a86d4dd75f7ba14dbb418
unknown <o.stefankina@g.nsu.ru>
2021-01-28 13:39:30 +0300

    create project

---
+++ 1704.py
@@ -0,0 +1,12 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        lenght = len(s) // 2
+        vowels_a, vowels_b = 0, 0
+        for i in range(0, lenght):
+            if s[i].lower() in 'aeiou':
+                vowels_a += 1
+        for i in range(lenght, len(s)):
+            if s[i].lower() in 'aeiou':
+                vowels_b += 1
+     
+        return vowels_a == vowels_b 
